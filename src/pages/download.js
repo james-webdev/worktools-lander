b613700dd24279367bc32c95c5a36793e1248fd6
James Roe <58564719+james-webdev@users.noreply.github.com>
2021-03-24 13:12:05 +0000

    debugging missing burger

--- src/pages/download.js
+++ src/pages/download.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Header from '../components/layout/Header';
+import DownLoadHeader from '../components/layout/Header';
 import Footer from '../components/layout/Footer';
 
 const Download = () => {
   return (
     <>
-      <Header />
+      <DownLoadHeader />
       <div className="text-center mt-20 mb-20">Download form</div>
       <div className="hidden sm:block">
         <Footer />
