ab20085695a9b35e5967f3a7e14fffb8d1b76660
James Roe <58564719+james-webdev@users.noreply.github.com>
2021-03-25 16:44:39 +0000

    worked on responsiveness, download working

--- src/pages/index.js
+++ src/pages/index.js
@@ -96,10 +96,10 @@ export default () => (
       </div>
     </section> */}
 
-    <section>
+    {/* <section>
       <div className="mt-20">
         <ContactBlue />
       </div>
-    </section>
+    </section> */}
   </Layout>
 );
