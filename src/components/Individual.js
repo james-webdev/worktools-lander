8489a11191de20af911d682039a16a7de5a5aff9
James Roe <58564719+james-webdev@users.noreply.github.com>
2021-04-08 10:41:50 +0100

    working on individual and company toggle pages

--- src/components/Individual.js
+++ src/components/Individual.js
@@ -4,7 +4,7 @@ const Individual = () => {
   return (
     <>
       <div className="text-center">
-        <span role="img">🧍</span>
+        <span role="img">👨</span>
       </div>
       <div className="font-bold poppins ml-4 mr-4 mt-6 mb-6 text-3xl">
         <p>Stay alert to security issues and become aware</p>
