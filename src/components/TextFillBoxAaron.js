3fd6ba29783c00458fe3580b2243162868f6393a
James Roe <58564719+james-webdev@users.noreply.github.com>
2021-05-03 11:00:25 +0100

    working on the desktop cyber essentials page

--- src/components/TextFillBoxAaron.js
+++ src/components/TextFillBoxAaron.js
@@ -1,4 +1,18 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { useSpring, animated } from "react-spring";
+
+const states = [
+  {
+    config: { duration: 300 },
+    from: { opacity: 0.1, x: -120 },
+    to: { opacity: 1, x:0}
+  },
+  {
+    config: { duration: 300},
+    from: { opacity: 0.1, x: -120  },
+    to: { opacity: 1, x: 0 }
+  }
+];
 
 const buttonData = [
   {
@@ -13,29 +27,51 @@ const buttonData = [
   {
     title: 'Business Interruption',
     content: 'Loss of profit and/or operational expenses caused by a network compromise.'
+  },
+{
+    title: 'Event management',
+    content:
+      'Costs, including emergency costs, following a data breach, including the costs of notifying data subjects. These might typically include payment for Legal, IT, Forensic & PR specialists.'
+  },
+  {
+    title: 'Regulatory Investigations',
+    content: 'Defence costs & regulatory fines (where insurable by law).'
+  },
+  {
+    title: 'Loss of Electronic Data',
+    content: 'Costs of remedying the issue that allowed the loss or damage to your data and costs to replace, restore or update your data.'
   }
+
 ];
 
 const HowItShouldBe = () => {
   const [mainText, setMainText] = useState(buttonData[0].content);
   const [selectedButton, setSelectedButton] = useState(buttonData[0].title);
+  const [toggle, setToggle] = useState(false);
+   const [props, set] = useSpring(() => ({
+    ...states[+toggle]
+  }));
 
   const buttons = () => {
     return buttonData.map((btn, index) => (
       <button
+      className="p-3 m-1 rounded-tr-lg rounded-bl-lg rounded-br-lg focus:outline-none"
         type="button"
         onClick={() => {
           setMainText(btn['content']);
           setSelectedButton(btn['title']);
+          set({
+                ...states[+!toggle]
+              });
         }}
         key={'button' + String(index)}
         style={{
-          backgroundColor: selectedButton === btn['title'] ? 'blue' : 'white'
+          backgroundColor: selectedButton === btn['title'] ? '#27aecc' : 'white'
         }}
       >
         <p
           style={{
-            color: selectedButton !== btn['title'] ? 'blue' : 'white'
+            color: selectedButton !== btn['title'] ? '#27aecc' : 'white'
           }}
         >
           {btn['title']}
@@ -45,12 +81,19 @@ const HowItShouldBe = () => {
   };
 
   return (
-    <div style={{ marginBottom: 50 }}>
+    
+    <div className="sm:flex sm:justify-center sm:items-center sm:mt-5">
+    <div className="mt-3 flex flex-col sm:flex sm:flex-row sm:flex-wrap sm:justify-center sm:items-center" >
       {buttons()}
-      <div className="tab1 text-white">
-        <p>{mainText}</p>
+      </div>
+      <div className="text-center tab1 sm:text-lg font-medium text-white rounded-tr-lg rounded-bl-lg rounded-br-lg sm:flex sm:justify-center sm:items-center m-3 p-2 pr-5 pl-5">
+        <animated.div style={props}>
+        <p className="">{mainText}</p>
+         </animated.div>
       </div>
     </div>
+    
+
   );
 };
 
--- src/pages/cyberessentials.js
+++ src/pages/cyberessentials.js
@@ -4,7 +4,7 @@ import DropdownMenu from '../components/DropDown';
 import GettingCertified from '../components/GettingCertified';
 import Footer from '../components/layout/Footer';
 import Header from '../components/layout/Header';
-import TextFillBox from '../components/TextFillBox';
+import TextFillBoxAaron from '../components/TextFillBoxAaron';
 import CyberEssentialsLogo from '../svg/CyberEssentialsLogo';
 import CyberEssentialsPlusLogo from '../svg/CyberEssentialsPlusLogo';
 import Kitty from '../svg/kitty';
@@ -14,75 +14,78 @@ const CyberEssentials = () => {
   return (
     <>
       <Header />
-      <div className="border border-black flex flex-col justify-center items-start p-4 mt-5 sm:flex">
+      <div className="flex flex-col justify-center items-start p-4 mt-5 sm:flex sm:ml-20 sm:mr-20">
         <div className="ml-5 mr-5">
-          <div className="sm:w-1/3">
-            <Kitty2 />
-          </div>
+          {/* <div className="sm:flex sm:justify-center sm:items-center">
+            <div className="sm:w-40 sm:mb-48 sm:mt-40 sm:flex sm:justify-center sm:items-center">
+              {/* <Kitty /> */}
+              {/* <Kitty2 />
+            </div>
+          </div> */} 
 
-          <div className="border border-black sm:flex">
-            <div className="border border-black text-4xl font-bold sm:text-5xl sm:w-1/3">
+          <div className="sm:flex">
+            <div className=" text-4xl font-bold sm:text-5xl sm:w-1/3">
               <h1>What is Cyber Essentials?</h1>
             </div>
 
-            <p className="border border-black mt-4 mr-10 border border-black sm:text-xl sm:w-2/3">
+            <p className="mt-4 mr-10  sm:text-xl sm:w-2/3">
               {' '}
               Cyber Essentials is a government-backed scheme helping you to guard your organisation,
               whatever its size, against a range of the most common cyber attacks.
             </p>
           </div>
         </div>
-        <div className="border border-black mt-10 mr-4 ml-4 sm:flex sm:flex-row-reverse sm:mt-20 sm:p-3 sm:items-center">
-          <div className="border border-black text-3xl font-bold sm:text-5xl">
+        <div className="mt-10 mr-4 ml-4 sm:flex sm:flex-row-reverse sm:mt-20 sm:p-3 sm:items-center">
+          <div className=" text-3xl font-bold sm:text-5xl sm:break-normal">
             {' '}
             <p>The two types of certification</p>
           </div>
-          <div className="border border-black mt-5 sm:mt-0 flex p-4">
-            <div className="border border-black">
+          <div className="mt-5 sm:mt-0 flex p-4">
+            <div className="">
               <CyberEssentialsLogo />
             </div>
-            <div className="border border-black text-base ml-2 flex justify-center items-center">
+            <div className="text-base ml-2 flex justify-center items-center">
               <p>Lightweight and easy to follow self-assessed certification.</p>
             </div>
           </div>
-          <div className="border border-black flex p-4">
-            <div className="border border-black">
+          <div className="flex p-4">
+            <div className="">
               <CyberEssentialsPlusLogo />
             </div>
-            <div className="border border-black text-base flex ml-2 justify-center items-center">
+            <div className="text-base flex ml-2 justify-center items-center">
               <p>Verified by independant Certification Body.</p>
             </div>
           </div>
           <div>
-            <p className="border border-black mr-16 mt-3 sm:hidden">
+            <p className="mr-16 mt-3 sm:hidden">
               Both options give protection against a wide variety of the most common cyber attacks.
             </p>{' '}
           </div>
         </div>
-        <div className="border border-black sm:mr-5 sm:ml-5 sm:mt-20">
+        <div className="sm:mr-5 sm:ml-5 sm:mt-20">
           <div className="text-center">
-            <h1 className="border border-black text-3xl ml-6 mr-6 mt-10 font-bold sm:text-5xl">
+            <h1 className=" text-3xl ml-6 mr-6 mt-10 font-bold sm:text-5xl">
               Why get Cyber Essentials?
             </h1>
           </div>
           <div className="sm:flex sm:justify-around sm:mt-6">
-            <div className="border border-black tab1 p-10 m-3 ml-5 mr-5 text-center text-lg font-medium text-white rounded-tr-lg rounded-bl-lg rounded-br-lg">
+            <div className=" tab1 p-10 m-3 ml-5 mr-5 text-center text-lg font-medium text-white rounded-tr-lg rounded-bl-lg rounded-br-lg">
               {' '}
               <p>Certified cyber security</p>
             </div>
-            <div className="border border-black tab2 p-10 m-3 ml-5 mr-5 text-center sm:text-lg font-medium text-white rounded-tr-lg rounded-bl-lg rounded-br-lg">
+            <div className="tab2 p-10 m-3 ml-5 mr-5 text-center sm:text-lg font-medium text-white rounded-tr-lg rounded-bl-lg rounded-br-lg">
               <p>Reassure customers that they are secure</p>
             </div>
-            <div className="border border-black tab3 p-10 m-3 ml-5 mr-5 text-center sm:text-lg font-medium text-white rounded-tr-lg rounded-bl-lg rounded-br-lg">
+            <div className="tab3 p-10 m-3 ml-5 mr-5 text-center sm:text-lg font-medium text-white rounded-tr-lg rounded-bl-lg rounded-br-lg">
               <p>Attract new business with evidence of cyber security measures in place</p>
             </div>
           </div>
-          <div className="border border-black sm:flex sm:justify-around sm:mt-4">
+          <div className="sm:flex sm:justify-around sm:mt-4">
             <div className="tab3 p-10 m-3 ml-5 mr-5 text-center sm:text-lg sm:w-1/3 font-medium text-white rounded-tr-lg rounded-bl-lg rounded-br-lg">
               {' '}
               <p>Have a clear picture of your organisation's security level</p>
             </div>
-            <div className="border border-black tab1 p-10 m-3 ml-5 mr-5 text-center sm:text-lg sm:w-1/3 font-medium text-white rounded-tr-lg rounded-bl-lg rounded-br-lg">
+            <div className="tab1 p-10 m-3 ml-5 mr-5 text-center sm:text-lg sm:w-1/3 font-medium text-white rounded-tr-lg rounded-bl-lg rounded-br-lg">
               <p>Some government contracts require Cyber Essentials certification</p>
             </div>
           </div>
@@ -206,12 +209,12 @@ const CyberEssentials = () => {
         <GettingCertified />
         <div />
       </div>
-      <div className="border border-black flex flex-col justify-center items-start poppins p-4 mt-5 ml-5 mr-5 sm:flex">
-        <div className="sm:flex sm:mt-20">
-          <h2 className="border border-black text-3xl font-bold sm:text-5xl sm:w-1/3">
+      <div className="flex flex-col justify-center items-start poppins p-4 mt-5 ml-5 mr-5 sm:ml-20 sm:mr-20 sm:flex">
+        <div className="sm:flex sm:justifycenter sm:items-center sm:mt-20">
+          <h2 className="text-3xl font-bold sm:text-5xl sm:w-1/3">
             Government Contracts
           </h2>
-          <p className=" border border-black mt-4 sm:w-2/3 sm:text-xl">
+          <p className="mt-4 sm:mt-0 sm:w-2/3 sm:text-xl">
             If you would like to bid for central government contracts which involve handling
             sensitive and personal information or the provision of certain technical products and
             services, you will require Cyber Essentials Certification. More information is available
@@ -221,20 +224,20 @@ const CyberEssentials = () => {
             </a>
           </p>
         </div>
-        <div className="border border-black sm:mt-20 sm:mb-20 sm:flex sm:flex-row-reverse sm:justify-around sm:p-3 sm:items-center">
-          <div className="sm:w-1/3 sm:text-right">
-            <p className="border border-black sm:text-5xl sm:mt-8 font-bold">
+        <div className="sm:mt-20 sm:mb-20 sm:flex sm:flex-row-reverse sm:justify-around sm:p-3 sm:items-center">
+          <div className="sm:w-1/3 sm:text-right sm:flex sm:flex-wrap sm:break-normal">
+            <p className="text-xl mt-5 sm:text-5xl sm:mt-8 font-bold">
               £25,000 of Cybersecurity Insurance Cover
             </p>
           </div>
-          <div className="border border-black sm:flex-col sm:w-2/3 sm:p-2">
+          <div className="sm:flex-col sm:w-2/3 sm:p-2">
             <div className="">
-              <p className="border border-black mt-4 sm:text-xl">
+              <p className="mt-4 sm:text-xl">
                 Having achieved Cyber Essentials compliance a company is then eligible for £25,000
                 of cybersecurity insurance cover from AXA that can be used for:
               </p>
             </div>
-            <TextFillBox />
+            <TextFillBoxAaron />
             {/* <div>
               {' '}
               <div className="border border-black sm:flex sm:justify-around sm:mt-6 sm:p-1">
@@ -299,6 +302,7 @@ const CyberEssentials = () => {
         </div>
       </div> */}
       <CyberBook />
+      <Footer />
     </>
   );
 };
